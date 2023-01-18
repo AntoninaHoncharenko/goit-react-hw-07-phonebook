39295d77747692729da391f6297e19eec088c217
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-01-18 23:00:57 +0200

    add asyncThunk operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import { GlobalStyle } from 'GlobalStyles';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operation';
+import { selectLoading, selectError } from 'redux/selectors';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Box } from './Box';
+import { Toaster } from 'react-hot-toast';
+import { Title, Subtitle } from './App.styled';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box width="800px" m="0 auto" p="30px">
+      <Title>Phonebook</Title>
+      <Box display="flex" justifyContent="space-around">
+        <Box>
+          <Subtitle>Contacts</Subtitle>
+          <Filter />
+          {isLoading && !error && <b>Loading...</b>}
+          <ContactList />
+        </Box>
+        <Box>
+          <Subtitle>Add contact</Subtitle>
+          <ContactForm />
+        </Box>
+      </Box>
+      <Toaster />
+      <GlobalStyle />
+    </Box>
   );
 };
